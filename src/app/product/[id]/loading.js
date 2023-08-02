d9606c8eaeba2ce740228be1f498df2abe608388
Amortha kumar shill <amitsharma1998k@gmail.com>
2023-08-02 20:34:40 +0600

    first commit

---
+++ src/app/product/[id]/loading.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="mt-32">
+      <h1 className=" text-5xl text-center font-bold">loading ......</h1>
+    </div>
+  );
+};
+
+export default loading;
